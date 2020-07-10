23aa61248448e9c7bfc35a2367ff928dd6688e17
GauthamHari000 <62849063+GauthamHari000@users.noreply.github.com>
2020-07-09 23:48:28 -0400

    Added gallery selection and dependency

--- lib/screens/VisualPage.dart
+++ lib/screens/VisualPage.dart
@@ -5,6 +5,7 @@ import 'package:ibm_visual_recog_img_file/connection.dart';
 import 'package:path/path.dart';
 import 'dart:async';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import 'package:path_provider/path_provider.dart';
 
@@ -16,8 +17,15 @@ class VisualPage extends StatefulWidget {
 class _VisualPageState extends State<VisualPage> {
   var _image;
   String _text;
-  Future getImage() async {
+  Future getImageFromCamera() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future getImageFromGallery() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
       _image = image;
     });
@@ -34,42 +42,57 @@ class _VisualPageState extends State<VisualPage> {
           Stack(
             children: [
               Container(
-                height: MediaQuery.of(context).size.height,
+                  height: MediaQuery.of(context).size.height,
                   width: double.infinity,
-                  child:_image == null ? Text('NO IMAGE') : new Image.file(_image)),
+                  child: _image == null
+                      ? Text('NO IMAGE')
+                      : new Image.file(_image)),
               Container(
                 child: StreamBuilder(
                     stream: StreamMyClassifier(
-                        _image, 'NRDjngCby2d-pSHOPyWQJxhuB6vOY2uOTCX6KV2BCfwB','CompostxLandfillxRecycle_2056123069'
-                    ),
-
+                        _image,
+                        'NRDjngCby2d-pSHOPyWQJxhuB6vOY2uOTCX6KV2BCfwB',
+                        'CompostxLandfillxRecycle_2056123069'),
                     builder: (context, snapshot) {
                       if (snapshot.hasData) {
                         _text = snapshot.data;
                         return Center(
                           child: Container(
-                            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/2),
-                            child: Text(_text,style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 33
-                            ),),),
+                            margin: EdgeInsets.only(
+                                top: MediaQuery.of(context).size.height / 2),
+                            child: Text(
+                              _text,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 33),
+                            ),
+                          ),
                         );
                       } else {
-                        return Container(
-
-                        );
+                        return Container();
                       }
                     }),
               )
-
             ],
           )
-
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        child: Icon(Icons.camera),
+      floatingActionButton: SpeedDial(
+        closeManually: true,
+        child: Icon(Icons.add_a_photo),
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.camera),
+              label: 'Camera',
+              onTap: () {
+                getImageFromCamera();
+              }),
+          SpeedDialChild(
+              child: Icon(Icons.image),
+              label: 'Gallery',
+              onTap: () {
+                getImageFromGallery();
+              })
+        ],
       ),
     );
   }
