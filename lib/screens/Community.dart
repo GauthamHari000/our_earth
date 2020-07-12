48cda1b5ba81a248b50d8dbd29b5eb71c236a8d2
GauthamHari000 <62849063+GauthamHari000@users.noreply.github.com>
2020-07-11 23:32:53 -0400

    Added Community Page

---
+++ lib/screens/Community.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CommunityHome extends StatefulWidget {
+  @override
+  _CommunityHomeState createState() => _CommunityHomeState();
+}
+
+class _CommunityHomeState extends State<CommunityHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
